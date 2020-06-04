40d029c49b17e519836fe84f6bfd18040245a98f
xabier hernandez <exeibier@MacBook-Pro-de-xabier.local>
2020-06-04 13:20:02 -0500

    add styled footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -16,12 +16,20 @@ function Footer (){
                         </div>
                     </div>
                     <div className='col-3 contact'>
-                        <p>Contacto</p>
-                        <FontAwesomeIcon icon={faPhone}/>
-                        <p>+55 31 313 121</p>
-                        <FontAwesomeIcon icon={faEnvelope}/>
-                        <p>fitu@fitu.mx</p>
-
+                        <div className='contact-p'>
+                          <p>Contacto</p>
+                        </div>
+                        <div className='icons' >
+                          <div className='content-contact d-flex justify-content-start'>
+                           <FontAwesomeIcon icon={faPhone} className='icon'/>
+                            <p> +55 31 313 121</p>
+                          </div>
+                          <div className='content-contact d-flex justify-content-start'>
+                           <FontAwesomeIcon icon={faEnvelope} className='icon'/>
+                            <p>fitu@fitu.mx</p>
+                          </div>
+                        </div>
+                       
                     </div>
                     <div className='col-6 social'>
                         <a href='#'><FontAwesomeIcon icon={faFacebook}/></a>
