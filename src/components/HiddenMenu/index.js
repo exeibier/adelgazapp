d67753fcc8d8a1dc1dd524d2a192eb80964865af
xabier hernandez <exeibier@MacBook-Pro-de-xabier.local>
2020-06-18 17:03:22 -0500

    minor fixes

--- src/components/HiddenMenu/index.js
+++ src/components/HiddenMenu/index.js
@@ -29,11 +29,11 @@ const HiddenMenu = props => {
                          {
                             !isUserLogedIn ? (
                              <li>
-                                <Link to="/signup" >Crear cuenta</Link>
+                                <Link to="/signup" className='btn-signup'>Crear cuenta</Link>
                              </li>
                              ) : (
                                 <li>
-                                <Link to="/activity" className='btn-signup' >Perfil</Link>
+                                <Link to="/activity" >Perfil</Link>
                              </li>   
                              )
                         } 
